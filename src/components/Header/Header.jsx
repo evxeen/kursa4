40b7d9505df37137da36d503161487235750d43f
evxeen <evxeen@gmail.com>
2022-09-19 22:52:25 +0300

    layout: add header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,28 @@
 import s from './Header.module.scss';
+import {useState} from "react";
 
 export const Header = () => {
+   const menuList = ['Каталог', 'О компании', 'Сотрудничество', 'Контакты'];
+   const [activeElement, setActiveElement] = useState(menuList[0]);
+
+
+   const selectActiveElement = (element) => {
+      setActiveElement(element);
+   };
 
    return (
        <section className={s.header}>
-          <div className={s.logo}>LOGO</div>
-
+          <div className={s.logo}>
+             <img src='/logo.png' alt='logo'/>
+          </div>
              <ul className={s.menu}>
-                <li>Каталог</li>
-                <li>О компании</li>
-                <li>Сотрудничество</li>
-                <li>Контакты</li>
+                {menuList.map((el) => (
+                    <li key={el} className={el === activeElement ? s.active : ''} onClick={() => selectActiveElement(el)}>{el}</li>
+                ))}
              </ul>
-
+          <div className={s.card}>
+             <img src="/4105931-add-to-cart-buy-cart-sell-shop-shopping-cart_113919.svg" alt="cart"/>
+          </div>
        </section>
    );
 };
