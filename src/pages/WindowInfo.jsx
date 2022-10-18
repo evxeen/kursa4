f693b23c42750856483f7f5144a42fd096be534d
evxeen <evxeen@gmail.com>
2022-10-19 00:25:11 +0300

    feat: add to cart

--- src/pages/WindowInfo.jsx
+++ src/pages/WindowInfo.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { addProduct } from "../store/slices/cartSlice";
 
 export const WindowInfo = () => {
+  const { cartList } = useSelector((state) => state.cartSlice);
   const { products } = useSelector((state) => state.productsSlice);
+  const dispatch = useDispatch();
   const [product, setProduct] = useState();
-
   const { id } = useParams();
 
   useEffect(() => {
@@ -17,6 +19,27 @@ export const WindowInfo = () => {
     return "Загрузка";
   }
 
+  const addToCart = (src, title, price, id) => {
+    const product = {
+      src,
+      title,
+      price,
+      id,
+      count: 1,
+    };
+
+    const checkingForAvailability = cartList.find(
+      (product) => product.id === id
+    );
+
+    if (checkingForAvailability) {
+      alert("Вы уже добавили этот товар в корзину");
+      return;
+    }
+
+    dispatch(addProduct(product));
+  };
+
   return (
     <div className="info_wrapper">
       <div className="info_image">
@@ -26,7 +49,13 @@ export const WindowInfo = () => {
         <p>{product.title}</p>
         <p>Цена: {product.price}</p>
         <p className="info_data-desc">{product.description}</p>
-        <button>В корзину</button>
+        <button
+          onClick={() =>
+            addToCart(product.src, product.title, product.price, product.id)
+          }
+        >
+          В корзину
+        </button>
       </div>
     </div>
   );
